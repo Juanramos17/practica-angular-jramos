98fb2c6f3fb9e5ebd8247ac7beb649af7db23f6f
Juanramos17 <juanramoscaro@gmail.com>
2025-03-10 18:45:50 +0100

    Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { PaginaNoEncontradaComponent } from './components/pagina-no-encontrada/pagina-no-encontrada.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidaComponent,
+    BusquedaComponent,
+    AcercaDeComponent,
+    PaginaNoEncontradaComponent
   ],
   imports: [
     BrowserModule,
